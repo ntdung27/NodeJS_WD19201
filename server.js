e91c6cffe75d25b331b134a2905682d88b68f689
thaivm-0609 <vuongthai95@gmail.com>
2024-11-07 11:13:26 +0700

    feat: ejs

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from "express"; //express dùng để khởi tạo node server
+import ejs from "ejs"; //hỗ trợ xây dựng giao diện
 
 const app = new express; //khởi tạo express
 const port = 3000; //khai báo cổng sẽ chạy node server
+
+//cấu hình ejs
+app.set('engine', 'ejs'); //khai báo template engine
+app.set('views', './views'); //khai báo đường dẫn đến thư mục chứa file giao diện
 
 app.use(
     express.urlencoded({
@@ -14,9 +19,22 @@ app.listen(port, () => {
     //app.method('/url', Controller);
     app.get('/list', (req,res) => {
         //gửi dữ liệu qua query: url có dạng ?query1=value1&query2=value2
-        console.log(req.query); 
-        res.send('<h1>Đây là trang danh sách</h1>');
+        let name = req.query.name;
+        let email = req.query.email; 
+        // res.send('<h1>Đây là trang danh sách</h1>'); res.send(): trả về string (có thể viết code html)
+        console.log(name);
+
+        //BT: Tạo 1 biến dạng array -> in dữ liệu của mảng đó ra giao diện
+        let array = [
+            { name: 'thaivm2', email: 'thaivm2@fpt.edu.vn'},
+            { name: 'thaivm3', email: 'thaivm3@fpt.edu.vn'},
+            { name: 'thaivm4', email: 'thaivm4@fpt.edu.vn'},
+            { name: 'thaivm5', email: 'thaivm5@fpt.edu.vn'},
+        ]
+
+        res.render('list.ejs', {array});
     })
+
     app.get('/detail/:name', (req,res) => {
         //gửi dữ liệu qua params: url có dạng /url/:tenParam
         console.log(req.params);
